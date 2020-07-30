9b5cd2d9aa8f66f6c97bc9c6581a43cd275a3c75
ymonb1291 <ymonb1291@gmail.com>
2020-07-30 22:41:18 +0200

    Add TODO on DEFAULT_ENV_IMPORT & DEFAULT_LOADABLE

--- constants.ts
+++ constants.ts
@@ -2,8 +2,10 @@ import { Configuration } from "./store.ts";
 import { ENVConfiguration } from "./cfg.ts";
 
 export const DEFAULT_ENV_EXPORT: boolean = false;
-export const DEFAULT_ENV_IMPORT: ENVConfiguration = { DEFAULT_ENV: true };
+// TODO: DEFAULT_ENV_IMPORT must be empty for release
+export const DEFAULT_ENV_IMPORT: ENVConfiguration = { __DEFAULT_ENV: "CONFIGURATION" };
 export const DEFAULT_ENV_MERGE: boolean = false;
 export const DEFAULT_ENV_PATH: string = ".env";
-export const DEFAULT_LOADABLE: Configuration = { defaultLoadable: 123 };
+// TODO: DEFAULT_LOADABLE must be empty for release
+export const DEFAULT_LOADABLE: Configuration = { __DEFAULT: "CONFIGURATION" };
 export const DEFAULT_SCOPE: string = "_DEFAULT";
